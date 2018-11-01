cb7b4cf746ee2c12bc8b30bbb291e1c2d8998302
Gabriel Capanema <capanema.ggfc@gmail.com>
2018-10-31 23:49:41 -0300

    build a router

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-const app = document.getElementById('app')
+import form from './pages/form';
+import list from './pages/list';
 
-app.innerHTML = '<div>TESTE</div>'
+const contentDiv = document.getElementById('app');
+
+const routes = {
+  '/form': form,
+  '/list': list
+};
+
+function validRoute() {
+  if (routes[window.location.pathname]) {
+    contentDiv.innerHTML = routes[window.location.pathname];
+  } else {
+    window.history.pushState({}, '/form', `${window.location.origin}/form`);
+  }
+}
+
+window.onpopstate = () => {
+  validRoute()
+}
+
+validRoute()
